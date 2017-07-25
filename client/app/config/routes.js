62fa650c45e9c686a7cd852c7f413591f8d25ff3
Salie Lim <salie.lim@gmail.com>
2017-07-25 12:55:34 +0800

    Organise client routes

--- client/app/config/routes.js
+++ client/app/config/routes.js
@@ -9,96 +9,173 @@
         $stateProvider
             // Public
             .state("index", {
-                url: "/index",
-                templateUrl: "app/index.html"
+                url: "/",
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/static/product.html"
+                    }
+                }
+                // resolve: {
+                //     user: function (PassportSvc) {
+                //         return PassportSvc.userAuth()
+                //             .then(function (result) {
+                //                 return result.data.user;
+                //             })
+                //             .catch(function (err) {
+                //                 return "";
+                //             });
+                //     }
+                // },
             })
             .state("product", {
                 url: "/product",
-                templateUrl: "app/static/product.html"
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/static/product.html"
+                    }
+                }
             })
             .state("pricing", {
                 url: "/pricing",
-                templateUrl: "app/static/pricing.html"
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/static/pricing.html"
+                    }
+                }
             })
             .state("contact", {
                 url: "/contact",
-                templateUrl: "app/static/contact.html"
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/static/contact.html"
+                    }
+                }
             })
             .state("login", {
                 url: "/login",
-                templateUrl: "app/login/login.html"
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/login/login.html"
+                    }
+                }
             })
             .state("register", {
                 url: "/register",
-                templateUrl: "app/register/register.html"
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/register/register.html"
+                    }
+                }
             })
             // .state("logout", {
             //     url: "/logout",
             // })
 
             // Protected
-            .state("scan", {
-                url: "/scan",
-                templateUrl: "app/protected/scan/scan.html",
-                resolve: {
-                    authenticated: checkLoggedin
-                }
-            })
             .state("data", {
                 url: "/data",
-                templateUrl: "app/protected/data/data.html",
-                resolve: {
-                    authenticated: checkLoggedin
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/protected/data/data.html",
+                        controller: "DataCtrl",
+                        controllerAs: "dataCtrl"
+                    }
+                    // resolve: {
+                    //     authenticated: checkLoggedin
+                    // }
                 },
-                controller: 'DataCtrl',
-                controllerAs: "dataCtrl"
             })
             .state("compare", {
                 url: "/compare",
-                templateUrl: "app/protected/compare/compare.html",
-                resolve: {
-                    authenticated: checkLoggedin
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/protected/compare/compare.html"
+                        // controller: "CompareCtrl",
+                        // controllerAs: "compareCtrl"
+                    }
                 }
             })
             .state("report", {
                 url: "/report",
-                templateUrl: "app/protected/report/report.html",
-                resolve: {
-                    authenticated: checkLoggedin
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/protected/report/report.html",
+                        // controller: "ReportCtrl",
+                        // controllerAs: "reportCtrl"
+                    }
                 }
             })
             .state("schedule", {
                 url: "/schedule",
-                templateUrl: "app/protected/schedule/schedule.html",
-                resolve: {
-                    authenticated: checkLoggedin
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/protected/schedule/schedule.html",
+                        // controller: "ScheduleCtrl",
+                        // controllerAs: "scheduleCtrl"
+                    }
                 }
             })
             .state("settings", {
                 url: "/settings",
-                templateUrl: "app/protected/settings/settings.html",
-                resolve: {
-                    authenticated: checkLoggedin
+                views: {
+                    'menu': {
+                        templateUrl: "app/menu/menu.html"
+                    },
+                    'content': {
+                        templateUrl: "app/protected/settings/settings.html",
+                        // controller: "SettingsCtrl",
+                        // controllerAs: "settingCtrl"
+                    }
                 }
             });
         $urlRouterProvider.otherwise("/");
     }
 
     var checkLoggedin = function ($q, $timeout, $http, $location, $rootScope) {
-            var deferred = $q.defer();
+        var deferred = $q.defer();
 
-            $http.get("/loggedin").then(function (user) {
-                $rootScope.errorMessage = null;
-                //User is Authenticated
-                if (user !== "0") {
-                    $rootScope.currentUser = user;
-                    deferred.resolve();
-                } else { //User is not Authenticated
-                    $rootScope.errorMessage = "You need to log in.";
-                    deferred.reject();
-                    $location.url("/login");
-                }
-            });
-            return deferred.promise;
-        }
+        $http.get("/loggedin").then(function (user) {
+            $rootScope.errorMessage = null;
+            //User is Authenticated
+            if (user !== "0") {
+                $rootScope.currentUser = user;
+                deferred.resolve();
+            } else { //User is not Authenticated
+                $rootScope.errorMessage = "You need to log in.";
+                deferred.reject();
+                $location.url("/login");
+            }
+        });
+        return deferred.promise;
+    }
 })();
