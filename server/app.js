29a3dee71f46fcfc63c41a1b5d5f5782b1f4c15a
Salie Lim <salie.lim@gmail.com>
2017-07-26 14:44:45 +0800

    Fix bson bug

--- server/app.js
+++ server/app.js
@@ -11,6 +11,9 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
+// Fix bson bug
+bson = require('../node_modules/mongoose/node_modules/bson/browser_build/bson.js');
+
 // Database
 var configDB = require('./config/database.js');
 mongoose.connect(process.env.MONGOLAB_URI || configDB.url);
