639ed4e70568bf2650e1aac69d5672cc02b77ea3
Salie Lim <salie.lim@gmail.com>
2017-08-09 20:36:14 +0800

    Tidy formatting with prettier

--- client/app/config/routes.js
+++ client/app/config/routes.js
@@ -1,231 +1,228 @@
-(function () {
-    angular
-        .module("SEO")
-        .config(uiRouteConfig);
+(function() {
+  angular.module("SEO").config(uiRouteConfig);
 
-    uiRouteConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
+  uiRouteConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
 
-    function uiRouteConfig($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            // Public
-            .state("index", {
-                url: "/",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/static/product.html"
-                    }
-                }
-            })
-            .state("product", {
-                url: "/product",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/static/product.html"
-                    }
-                }
-            })
-            .state("pricing", {
-                url: "/pricing",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/static/pricing.html"
-                    }
-                }
-            })
-            .state("contact", {
-                url: "/contact",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/static/contact.html"
-                    }
-                }
-            })
-            .state("login", {
-                url: "/login",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/login/login.html"
-                    }
-                }
-            })
-            .state("register", {
-                url: "/register",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/register/register.html"
-                    }
-                }
-            })
-
-            // Protected
-            .state("data", {
-                url: "/data",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/protected/data/data.html",
-                        controller: "DataCtrl",
-                        controllerAs: "dataCtrl"
-                    }
-                },
-                resolve: {
-                    user: function (AuthService) {
-                        return AuthService.userAuth()
-                            .then(function (result) {
-                                return result.data.user;
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return '';
-                            });
-                    }
-                }
-            })
-            .state("scan", {
-                url: "/scan",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/protected/scan/scan.html",
-                        controller: "ScanCtrl",
-                        controllerAs: "scanCtrl"
-                    }
-                },
-                resolve: {
-                    user: function (AuthService) {
-                        return AuthService.userAuth()
-                            .then(function (result) {
-                                return result.data.user;
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return '';
-                            });
-                    }
-                }
-            })
-            .state("compare", {
-                url: "/compare",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/protected/compare/compare.html"
-                        // controller: "CompareCtrl",
-                        // controllerAs: "compareCtrl"
-                    }
-                },
-                resolve: {
-                    user: function (AuthService) {
-                        return AuthService.userAuth()
-                            .then(function (result) {
-                                return result.data.user;
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return '';
-                            });
-                    }
-                }
-            })
-            .state("report", {
-                url: "/report",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/protected/report/report.html",
-                        controller: "ReportCtrl",
-                        controllerAs: "reportCtrl"
-                    }
-                },
-                resolve: {
-                    user: function (AuthService) {
-                        return AuthService.userAuth()
-                            .then(function (result) {
-                                return result.data.user;
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return '';
-                            });
-                    }
-                }
-            })
-            .state("schedule", {
-                url: "/schedule",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/protected/schedule/schedule.html",
-                        controller: "ScheduleCtrl",
-                        controllerAs: "scheduleCtrl"
-                    }
-                },
-                resolve: {
-                    user: function (AuthService) {
-                        return AuthService.userAuth()
-                            .then(function (result) {
-                                return result.data.user;
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return '';
-                            });
-                    }
-                }
-            })
-            .state("settings", {
-                url: "/settings",
-                views: {
-                    'menu': {
-                        templateUrl: "app/menu/menu.html"
-                    },
-                    'content': {
-                        templateUrl: "app/protected/settings/settings.html",
-                        controller: "SettingsCtrl",
-                        controllerAs: "settingCtrl"
-                    }
-                },
-                resolve: {
-                    user: function (AuthService) {
-                        return AuthService.userAuth()
-                            .then(function (result) {
-                                return result.data.user;
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                                return '';
-                            });
-                    }
-                }
-            });
-        $urlRouterProvider.otherwise("/");
-    }
+  function uiRouteConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      // Public
+      .state("index", {
+        url: "/",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/static/product.html"
+          }
+        }
+      })
+      .state("product", {
+        url: "/product",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/static/product.html"
+          }
+        }
+      })
+      .state("pricing", {
+        url: "/pricing",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/static/pricing.html"
+          }
+        }
+      })
+      .state("contact", {
+        url: "/contact",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/static/contact.html"
+          }
+        }
+      })
+      .state("login", {
+        url: "/login",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/login/login.html"
+          }
+        }
+      })
+      .state("register", {
+        url: "/register",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/register/register.html"
+          }
+        }
+      })
+      // Protected
+      .state("data", {
+        url: "/data",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/protected/data/data.html",
+            controller: "DataCtrl",
+            controllerAs: "dataCtrl"
+          }
+        },
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                console.log(err);
+                return "";
+              });
+          }
+        }
+      })
+      .state("scan", {
+        url: "/scan",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/protected/scan/scan.html",
+            controller: "ScanCtrl",
+            controllerAs: "scanCtrl"
+          }
+        },
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                console.log(err);
+                return "";
+              });
+          }
+        }
+      })
+      .state("compare", {
+        url: "/compare",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/protected/compare/compare.html"
+            // controller: "CompareCtrl",
+            // controllerAs: "compareCtrl"
+          }
+        },
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                console.log(err);
+                return "";
+              });
+          }
+        }
+      })
+      .state("report", {
+        url: "/report",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/protected/report/report.html",
+            controller: "ReportCtrl",
+            controllerAs: "reportCtrl"
+          }
+        },
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                console.log(err);
+                return "";
+              });
+          }
+        }
+      })
+      .state("schedule", {
+        url: "/schedule",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/protected/schedule/schedule.html",
+            controller: "ScheduleCtrl",
+            controllerAs: "scheduleCtrl"
+          }
+        },
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                console.log(err);
+                return "";
+              });
+          }
+        }
+      })
+      .state("settings", {
+        url: "/settings",
+        views: {
+          menu: {
+            templateUrl: "app/menu/menu.html"
+          },
+          content: {
+            templateUrl: "app/protected/settings/settings.html",
+            controller: "SettingsCtrl",
+            controllerAs: "settingCtrl"
+          }
+        },
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.userAuth()
+              .then(function(result) {
+                return result.data.user;
+              })
+              .catch(function(err) {
+                console.log(err);
+                return "";
+              });
+          }
+        }
+      });
+    $urlRouterProvider.otherwise("/");
+  }
 })();
